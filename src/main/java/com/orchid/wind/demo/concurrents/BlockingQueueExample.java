2f8143443d9d1d1d85ad0d1e34002d6108addb61
hughChen <shuliyuanshi@163.com>
2020-07-11 20:44:12 +0800

    [add] blocking queue

---
+++ src/main/java/com/orchid/wind/demo/concurrents/BlockingQueueExample.java
@@ -0,0 +1,58 @@
+package com.orchid.wind.demo.concurrents;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class BlockingQueueExample {
+    public static void main(String[] args) throws Exception{
+        ArrayBlockingQueue queue = new ArrayBlockingQueue(1024);
+        Producer producer = new Producer(queue);
+        Consumer consumer = new Consumer(queue);
+        new Thread(producer).start();
+        new Thread(consumer).start();
+        Thread.sleep(4000);
+    }
+}
+
+class Producer implements Runnable {
+    protected ArrayBlockingQueue queue = null;
+
+    public Producer(ArrayBlockingQueue queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            queue.put("1");
+            System.out.println("put" + 1);
+            Thread.sleep(1000);
+            queue.put("2");
+            System.out.println("put" + 2);
+            Thread.sleep(1000);
+            queue.put("3");
+            System.out.println("put" + 3);
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class Consumer implements Runnable {
+    protected ArrayBlockingQueue queue = null;
+
+    public Consumer(ArrayBlockingQueue queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {
+        try {
+            System.out.println("take" + queue.take());
+            System.out.println("take" + queue.take());
+            System.out.println("take" + queue.take());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
